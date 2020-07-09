b201485e651db9185b3a1771bb56881bdd41bdf5
ViryGV <viridiana_gutierrez@hotmail.com>
2020-07-08 19:47:29 -0500

    Agregado el script ControladorPaneles
    El script sirve para manejar el flujo entre los diferentes menus de la apliación (ir de un menú a otro o regresar al menú principal)

---
+++ Aplicacion/Assets/Scripts/ControladorPaneles.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPaneles : MonoBehaviour
+{
+    public GameObject panelPrincipal;
+    public GameObject panelJuegos;
+    public GameObject panelSalir;
+    public GameObject panelInfo;
+    public GameObject panelOpciones;
+    public GameObject panelActividades;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivarPanelPrincipal()
+    {
+        panelPrincipal.SetActive (true);
+        panelJuegos.SetActive(false); //Mundos
+    }
+
+    public void DesactivarPanelPrincipal()
+    {
+        panelPrincipal.SetActive(false);
+    }
+
+    public void ActivarPanelJuegos()
+    {
+        panelJuegos.SetActive(true);
+        //panelPrincipal.SetActive(false);
+    }
+
+    public void DesactivarPanelJuegos()
+    {
+        panelPrincipal.SetActive(true);
+        panelJuegos.SetActive(false);
+    }
+
+    public void ActivarPanelOpciones() //Personajes
+    {
+        panelOpciones.SetActive(true);
+        //panelPrincipal.SetActive(false);
+    }
+
+    public void DesactivarPanelOpciones()
+    {
+        panelPrincipal.SetActive(true);
+        panelOpciones.SetActive(false);
+    }
+
+    public void ActivarPanelInfo()
+    {
+        panelInfo.SetActive(true);
+        //panelPrincipal.SetActive(false);
+    }
+
+    public void DesactivarPanelInfo()
+    {
+        panelPrincipal.SetActive(true);
+        panelInfo.SetActive(false);
+    }
+
+    public void ActivarPanelSalir()
+    {
+        panelSalir.SetActive(true);
+        //panelPrincipal.SetActive(false);
+    }
+
+    public void DesactivarPanelSalir()
+    {
+        panelPrincipal.SetActive(true);
+        panelSalir.SetActive(false);
+    }
+}
