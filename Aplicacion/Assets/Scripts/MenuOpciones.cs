c5853c3e39742a3fa7bbd0aed53db118018e14e7
ViryGV <viridiana_gutierrez@hotmail.com>
2020-07-10 19:52:22 -0500

    Agregado el script para las opciones
    El script de opciones será ignorado por el momento al ser una funcionalidad complemetaria
    Se añade el componete de ajustar volumen

---
+++ Aplicacion/Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpciones : MonoBehaviour
+{
+    public void SetVolume (float volumen)
+    {
+        Debug.Log(volumen);
+    }
+}
