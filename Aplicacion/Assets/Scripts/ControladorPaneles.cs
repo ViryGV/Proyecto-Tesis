13ffe4321b52b56c54184f0c04b9db47f3c2755d
ViryGV <viridiana_gutierrez@hotmail.com>
2020-07-18 16:50:56 -0500

    Comentarios en el script ControladorPaneles
    Se le agrega una descripción del funcionamiento del script

--- Aplicacion/Assets/Scripts/ControladorPaneles.cs
+++ Aplicacion/Assets/Scripts/ControladorPaneles.cs
@@ -2,6 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+///<summary>
+///Clase cuya función es controlar el flujo entre los distintos paneles (menús) dispuestos para la aplicación
+///<para>Permite la navegación entre las opciones existentes</para>
+///</summary>
+///<list type="table">
+///<item>
+///<term>ActivarPanelPrincipal</term>
+///<description>Activa la vista de las opciones y funciones principlaes</description>
+///<item>
+///<term>DesactivarPanelPrincipal</term>
+///<description>Desactiva la vista de las opciones y funciones principlaes para dar acceso a otro panel</description>
+///<term>ActivarPanelJuegos</term>
+///<description>Activa la vista de las opciones para las actividades (Historias, Practicar, Profesiones y Juegos)</description>
+///<item>
+///<term>DesactivarPanelJuegos</term>
+///<description>Desactiva la vista del panel para dar acceso a otro panel o al panel principal</description>
+///<item>
+///<term>ActivarPanelOpciones</term>
+///<description>Activa la vista de las opciones de la aplicación</description>
+///<item>
+///<term>DesactivarPanelOpciones</term>
+///<description>Desactiva la vista de las opciones para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelInfo</term>
+///<description>Activa la vista del panel de información donde se describe a la aplicación, su desarrollador y contribuciones</description>
+///<item>
+///<term>DesactivarPanelInfo</term>
+///<description>Desactiva la vista de la información para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelSalir</term>
+///<description>Activa la vista para la opción de salir (cerrar la aplicación)</description>
+///<item>
+///<term>DesactivarPanelSalir</term>
+///<description>Desactiva la vista de salir para dar acceso al panel principal o cerrar la aplicación</description>
+///<item>
+///<term>ActivarPanelProgreso</term>
+///<description>Activa la vista para ver el progreso de los niños</description>
+///<item>
+///<term>DesactivarPanelProgreso</term>
+///<description>Desactiva la vista de progreso para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelActividades</term>
+///<description>Activa la vista de las actividades recompiladas en base a estrategias</description>
+///<item>
+///<term>DesactivarPanelActiviades</term>
+///<description>Desactiva la vista de las activadades para dar acceso al panel de juegos</description>
+///<item>
+///<term>DesactivarPanelActividadesJuegos</term>
+///<description>Desactiva la vista de juegos para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelHistorias</term>
+///<description>Activa la vista de algunas historias donde el niño puede seleccionar una respuesta ante la situación presentada</description>
+///<item>
+///<term>DesactivarPanelHistorias</term>
+///<description>Desactiva la vista de las historias para dar acceso al panel de juegos</description>
+///<item>
+///<term>DesactivarPanelHistoriasJuegos</term>
+///<description>Desactiva la vista de juegos para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelProfesiones</term>
+///<description>Activa la vista de algunas profesiones u oficios donde se le permita al niño generar interés por otros temas</description>
+///<item>
+///<term>DesactivarPanelProfesiones</term>
+///<description>Desactiva la vista de las profesioes y oficios para dar acceso al panel de juegos</description>
+///<item>
+///<term>DesactivarPanelProfesionesJuegos</term>
+///<description>Desactiva la vista de juegos para dar acceso al panel principal</description>
+///<item>
+///<term>ActivarPanelMatematicas</term>
+///<description>Activa la vista que le permite al niño poner en práctica sus hábilidades y conocimiento en el área de matemáticas</description>
+///<item>
+///<term>DesactivarPanelMatematicas</term>
+///<description>Desactiva la vista de matemáticas para dar acceso al panel de juegos</description>
+///<item>
+///<term>DesactivarPanelMatematicasJuegos</term>
+///<description>Desactiva la vista de juegos para dar acceso al panel principal</description>
 public class ControladorPaneles : MonoBehaviour
 {
     public GameObject panelPrincipal;
