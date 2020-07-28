6fd5a9fa903cfc717049d1ec120abf0ff73cacff
ViryGV <viridiana_gutierrez@hotmail.com>
2020-07-27 20:14:56 -0500

    Agregado Scrip para manejar instrucciones
    Se agregan los scripts que controlan el tiempo de vida de cosmo y la burbuja de mensaje
    Se crea una copia del Script ControladorDialogo para adaptarlo al nuevo nivel

---
+++ Aplicacion/Assets/Scripts/ControladorDialogoNoP.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//compile with: -doc:DocFileName.xml
+
+/// <summary>
+/// Esta clase maneja el flujo de las conversaciones de Cosmo
+/// Y maneja cuando aparecen los objetos del nivel
+/// </summary>
+public class ControladorDialogoNoP : MonoBehaviour
+{
+    /// <value>Establece el valor del texto que será mostrado en pantalla</value>
+    public TextMeshProUGUI textoDisplay;
+    /// <value>Establece el valor para poder seguir avanzando en la conversación</value>
+    public GameObject botonContinuar;
+    public GameObject textoAciertos;
+
+    /// <value>Establece el valor para poner sonido en la conversación</value>
+    private AudioSource _audio;
+
+    //ControladorPuntos controladorPuntos;
+
+    /// <value>Crea un arreglo para las sentencias del diálogo</value>
+    public string[] palabras;
+    /// <value>Establece el valor de la velocidad en el que cargan las oraciones</value>
+    public float typingSpeed;
+
+    /// <value>Permite conocer la posición de las palabras</value>
+    private int _index;
+
+    /// <summary>
+    /// Inicializa las variables establecidas
+    /// </summary>
+    void Start()
+    {
+        //_audio = GetComponent<AudioSource>();
+        StartCoroutine(Type());
+    }
+
+    /// <summary>
+    /// Actualiza los valores de las variables establecidas
+    /// </summary>
+    void Update()
+    {
+        // Si el texto mostrado en pantalla es igual a la posición 
+        // de las palabras en el arreglo,
+        // el botón para continuar la conversación se activa
+        if (textoDisplay.text == palabras[_index])
+        {
+            botonContinuar.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Permite crear una corrutina entre el lapso de tiempo en que
+    /// se muestran el diálogo actual y el nuevo diálogo a mostrar
+    /// </summary>
+    /// <remarks>
+    /// Mediante el foreach se recorren las palabras de la oración 
+    /// que se esta llevando a cabo
+    /// </remarks>
+    IEnumerator Type()
+    {
+        foreach (char letras in palabras[_index].ToCharArray())
+        {
+            textoDisplay.text += letras;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Actualiza la conversación a la siguiente oración
+    /// </summary>
+    public void NextSentence()
+    {
+        //_audio.Play();
+        botonContinuar.SetActive(false);
+        textoAciertos.SetActive(false);
+
+        // Si el índice de las palabras es menor al tamaño de ellas 
+        // se incrementa un valor, los objetos permanecen desabilitados
+        // y se llama a la corrutina
+        // De lo contrario se desactiva el botón para continuar el diálogo
+        // y se habilita el resto de los componentes
+        if (_index < palabras.Length - 1)
+        {
+            _index++;
+            textoDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textoDisplay.text = "";
+            botonContinuar.SetActive(false);
+            textoAciertos.SetActive(true);
+        }
+    }
+}
---
+++ Aplicacion/Assets/Scripts/ControladorPuntosANivU.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControladorPuntosANivU : MonoBehaviour
+{
+    public TextMeshProUGUI textoAciertos;
+    //public Text textoErrores;
+    public TextMeshProUGUI textoMensaje;
+    public GameObject cosmo;
+    public GameObject panelAlegria;
+    public GameObject panelEnojo;
+    public GameObject panelTristeza;
+    public GameObject panelSopresa;
+    public GameObject panelTemor;
+    public GameObject panelAburrido;
+    public GameObject panelPreocupado;
+    public GameObject panelCansado;
+    public GameObject objetoAlegria;
+    public GameObject objetoEnojo;
+    public GameObject objetoTristeza;
+    public GameObject objetoSorpresa;
+    public GameObject objetoTemor;
+    public GameObject objetoAburrido;
+    public GameObject objetoPreocupado;
+    public GameObject objetoCansado;
+
+    int puntajeAciertos;
+    int puntajeErrores;
+
+    ControladorJuego controladorJuego;
+    /// <value>Acceder al valor de Renderer en los objetos para manipularlos</value>
+    Renderer render;
+    Renderer renderE;
+    Renderer renderT;
+    Renderer renderS;
+    Renderer renderA;
+    Renderer renderP;
+    Renderer renderC;
+    Renderer renderTemor;
+
+    void Start()
+    {
+        render = objetoAlegria.GetComponent<Renderer>();
+        renderE = objetoEnojo.GetComponent<Renderer>();
+        renderT = objetoTristeza.GetComponent<Renderer>();
+        renderS = objetoSorpresa.GetComponent<Renderer>();
+        renderA = objetoAburrido.GetComponent<Renderer>();
+        renderP = objetoPreocupado.GetComponent<Renderer>();
+        renderTemor = objetoTemor.GetComponent<Renderer>();
+        renderC = objetoCansado.GetComponent<Renderer>();
+    }
+
+    void Awake()
+    {
+        controladorJuego = GameObject.Find("ControladorJuego").GetComponent(typeof(ControladorJuego)) as ControladorJuego;
+    }
+
+    public void SumarPuntaje()
+    {
+        puntajeAciertos += 1;
+        ActualizarTextoPuntaje();
+        ActivarStatus();
+
+        if (puntajeAciertos >= 1)
+        {
+            Debug.Log("puntaje 1");
+            DesactivarPanelAlegria();
+            ActivarPanelEnojo();
+        }
+        if (puntajeAciertos >= 2)
+        {
+            Debug.Log("puntaje 2");
+            DesactivarPanelEnojo();
+            ActivarPanelTristeza();
+        }
+        if (puntajeAciertos >= 3)
+        {
+            Debug.Log("puntaje 3");
+            DesactivarPanelTristeza();
+            ActivarPanelSorpresa();
+        }
+        if (puntajeAciertos >= 4)
+        {
+            Debug.Log("puntaje 4");
+            DesactivarPanelSorpresa();
+            ActivarPanelTemor();
+        }
+        if (puntajeAciertos >= 5)
+        {
+            Debug.Log("puntaje 5");
+            DesactivarPanelTemor();
+            ActivarPanelAburrido();
+        }
+        if (puntajeAciertos >= 6)
+        {
+            Debug.Log("puntaje 6");
+            DesactivarPanelAburrido();
+            ActivarPanelPreocupado();
+        }
+        if (puntajeAciertos >= 7)
+        {
+            Debug.Log("puntaje 7");
+            DesactivarPanelPreocupado();
+            ActivarPanelCansado();
+        }
+
+        if (puntajeAciertos >= 8)
+        {
+            controladorJuego.DesbloquearNivel();
+        }
+    }
+
+    public void RestarPuntaje()
+    {
+        puntajeErrores += 1;
+        ActualizarTextoPuntaje();
+        ActivarPistas();
+    }
+
+    void ActualizarTextoPuntaje()
+    {
+        textoAciertos.text = "Aciertos " + puntajeAciertos.ToString();
+        //textoErrores.text = "Errores " + puntajeErrores.ToString();
+    }
+
+    public void ActivarStatus()
+    {
+        textoMensaje.enabled = true;
+        cosmo.SetActive(true);
+        textoMensaje.text = "Bien Hecho";
+    }
+
+    public void ActivarPistas()
+    {
+        textoMensaje.enabled = true;
+        cosmo.SetActive(true);
+        textoMensaje.text = "Intentalo de nuevo";
+    }
+
+    public void ActivarPanelAlegria()
+    {
+        panelAlegria.SetActive(true);
+        render.enabled = true;
+    }
+
+    public void DesactivarPanelAlegria()
+    {
+        panelAlegria.SetActive(false);
+        render.enabled = false;
+    }
+
+    public void ActivarPanelEnojo()
+    {
+        panelEnojo.SetActive(true);
+        renderE.enabled = true;
+    }
+
+    public void DesactivarPanelEnojo()
+    {
+        panelEnojo.SetActive(false);
+        renderE.enabled = false;
+    }
+
+    public void ActivarPanelTristeza()
+    {
+        panelTristeza.SetActive(true);
+        renderT.enabled = true;
+    }
+
+    public void DesactivarPanelTristeza()
+    {
+        panelTristeza.SetActive(false);
+        renderT.enabled = false;
+    }
+
+    public void ActivarPanelSorpresa()
+    {
+        panelSopresa.SetActive(true);
+        renderS.enabled = true;
+    }
+
+    public void DesactivarPanelSorpresa()
+    {
+        panelSopresa.SetActive(false);
+        renderS.enabled = false;
+    }
+
+    public void ActivarPanelTemor()
+    {
+        panelTemor.SetActive(true);
+        renderTemor.enabled = true;
+    }
+
+    public void DesactivarPanelTemor()
+    {
+        panelTemor.SetActive(false);
+        renderTemor.enabled = false;
+    }
+
+    public void ActivarPanelAburrido()
+    {
+        panelAburrido.SetActive(true);
+        renderA.enabled = true;
+    }
+
+    public void DesactivarPanelAburrido()
+    {
+        panelAburrido.SetActive(false);
+        renderA.enabled = false;
+    }
+
+    public void ActivarPanelPreocupado()
+    {
+        panelPreocupado.SetActive(true);
+        renderP.enabled = true;
+    }
+
+    public void DesactivarPanelPreocupado()
+    {
+        panelPreocupado.SetActive(false);
+        renderP.enabled = false;
+    }
+
+    public void ActivarPanelCansado()
+    {
+        panelCansado.SetActive(true);
+        renderC.enabled = true;
+    }
+
+    public void DesactivarPanelCansado()
+    {
+        panelCansado.SetActive(false);
+        renderC.enabled = false;
+    }
+}
